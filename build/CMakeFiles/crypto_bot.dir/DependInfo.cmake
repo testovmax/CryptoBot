646c4e58e61a96af5a4982d51e5a5fcc1e149330
maximkin22 <136425708+maximkin22@users.noreply.github.com>
2025-12-09 21:36:27 +0300

    first_commit

---
+++ build/CMakeFiles/crypto_bot.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/prj/VoGU_course_2/CryptoBot/src/CryptoBot.cpp" "CMakeFiles/crypto_bot.dir/src/CryptoBot.cpp.o" "gcc" "CMakeFiles/crypto_bot.dir/src/CryptoBot.cpp.o.d"
+  "/mnt/c/prj/VoGU_course_2/CryptoBot/src/CurrencyManager.cpp" "CMakeFiles/crypto_bot.dir/src/CurrencyManager.cpp.o" "gcc" "CMakeFiles/crypto_bot.dir/src/CurrencyManager.cpp.o.d"
+  "/mnt/c/prj/VoGU_course_2/CryptoBot/src/TelegramHandler.cpp" "CMakeFiles/crypto_bot.dir/src/TelegramHandler.cpp.o" "gcc" "CMakeFiles/crypto_bot.dir/src/TelegramHandler.cpp.o.d"
+  "/mnt/c/prj/VoGU_course_2/CryptoBot/src/UserManager.cpp" "CMakeFiles/crypto_bot.dir/src/UserManager.cpp.o" "gcc" "CMakeFiles/crypto_bot.dir/src/UserManager.cpp.o.d"
+  "/mnt/c/prj/VoGU_course_2/CryptoBot/src/main.cpp" "CMakeFiles/crypto_bot.dir/src/main.cpp.o" "gcc" "CMakeFiles/crypto_bot.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/crypto_bot.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/crypto_bot.dir/src/CryptoBot.cpp.o"
+  "CMakeFiles/crypto_bot.dir/src/CryptoBot.cpp.o.d"
+  "CMakeFiles/crypto_bot.dir/src/CurrencyManager.cpp.o"
+  "CMakeFiles/crypto_bot.dir/src/CurrencyManager.cpp.o.d"
+  "CMakeFiles/crypto_bot.dir/src/TelegramHandler.cpp.o"
+  "CMakeFiles/crypto_bot.dir/src/TelegramHandler.cpp.o.d"
+  "CMakeFiles/crypto_bot.dir/src/UserManager.cpp.o"
+  "CMakeFiles/crypto_bot.dir/src/UserManager.cpp.o.d"
+  "CMakeFiles/crypto_bot.dir/src/main.cpp.o"
+  "CMakeFiles/crypto_bot.dir/src/main.cpp.o.d"
+  "crypto_bot"
+  "crypto_bot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/crypto_bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
